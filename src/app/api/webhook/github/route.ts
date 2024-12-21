7e5106b027a6247f3a5eda0fc2e62d463c45ac6e
yash-devop <kamble1234meena@gmail.com>
2024-12-21 09:04:01 +0530

    add: prisma and octokit webhook event listeners

--- src/app/api/webhook/github/route.ts
+++ src/app/api/webhook/github/route.ts
@@ -1,34 +1,40 @@
-// pages/api/webhook/github.js
 import githubClient from "@/app/lib/octokit";
+import prisma from "@/app/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
-// export const config = {
-//   api: {
-//     bodyParser: false, // Disable Next.js body parsing for raw JSON access
-//   },
-// };
+githubClient.webhooks.on("installation.created", async (data) => {
+  console.log("INSTALLATION IN POST BLOCK", data.payload);
+  const { payload } = data;
+  const created = await prisma.owner.create({
+    data: {
+      installationId: String(payload.installation.id),
+      // @ts-ignore
+      owner: String(payload.installation.account.login),
+    },
+  });
 
-githubClient.webhooks.on("installation.created", async ({ payload }) => {
-  console.log("payload of installation:", payload);
-//   return NextResponse.redirect("/dashboard/success", { status: 302 });
+  console.log("CREATED : ", created);
 });
-githubClient.webhooks.on("issues", async ({ payload }) => {
-  console.log("payload of issues:", payload);
+
+githubClient.webhooks.on("installation.deleted", async ({ payload }) => {
+  console.log("app delete block => ");
+  // await prisma.owner.delete({
+  //   where: {
+  //     // @ts-ignore
+  //     owner: String(payload.installation.account.login),
+  //   },
+  // });
 });
 
 export const POST = async (req: NextRequest) => {
   try {
     const body = await req.text(); // Read the request body as a string
-    console.log("raw body for this request is:", body);
-
-    console.log('headers: ' , req.headers);
     await githubClient.webhooks.verifyAndReceive({
-      id: req.headers.get("x-github-delivery") as string, // Unique event ID from GitHub
+      id: req.headers.get("x-github-delivery") as string, // Unique event ID from GitHub API
       name: req.headers.get("x-github-event") as any, // Event type (e.g., "push")
       signature: req.headers.get("x-hub-signature-256") as string, // HMAC signature for validation
       payload: body, // Pass the raw body for signature verification
     });
-
     return NextResponse.json(
       {
         message: "Webhook is verified !!",
@@ -37,55 +43,10 @@ export const POST = async (req: NextRequest) => {
         status: 200,
       }
     );
-  } catch (error) {
-    console.log("error in app install: ", error);
+  } catch (error: any) {
     return NextResponse.json({
       message: "Error while installing app in your repository",
       error,
     });
   }
 };
-
-
-
-// import githubClient from "@/app/lib/octokit"
-// import { NextRequest, NextResponse } from "next/server"
-
-// export const config = {
-//     api: {
-//       bodyParser: false, // Disable Next.js body parsing for raw JSON access
-//     },
-//   };
-
-// export const POST=async(req:NextRequest)=>{
-//     try {
-//         const eventType = req.headers.get("X-GitHub-Event"); // Get event type from header
-//         const rawBody = await req.text(); // Read raw request body
-//         const body = JSON.parse(rawBody); // Parse JSON payload
-
-//         if (eventType === "installation") {
-//           console.log('Payload of installation:', body);
-//           const installationId = body.installation.id;
-//           const account = body.installation.account;
-//           // Further processing based on installation data
-
-//           return NextResponse.redirect("/dashboard/success", { status: 302 });
-//         } else if (eventType === "issues") {
-//           console.log('Payload of issues:', body);
-//           const issue = body.issue; // Assuming the issue object is directly in the payload
-//           // Handle issues event
-
-//           return NextResponse.json({ message: "Handled issues event." }, { status: 200 });
-//         } else {
-//           console.warn(`Unsupported event type: ${eventType}`);
-//           return NextResponse.json({ message: "Unsupported event type." }, { status: 400 });
-//         }
-//       } catch (error) {
-//         console.error('Error in app install: ', error);
-//         return NextResponse.json({
-//           message: "Error while installing app in your repository",
-//           error,
-//         });
-//       }
-
-// }
