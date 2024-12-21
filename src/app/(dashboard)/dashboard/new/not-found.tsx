b1643de932b22b85023f07715818b156b8b5917b
yash-devop <kamble1234meena@gmail.com>
2024-12-21 08:59:19 +0530

    add: notfound page for dashboard creation page

---
+++ src/app/(dashboard)/dashboard/new/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound(){
+    return (
+        <>
+            <p>Installed APP not found... Please check whether you have installation the app or authenticated.</p>
+        </>
+    )
+}
