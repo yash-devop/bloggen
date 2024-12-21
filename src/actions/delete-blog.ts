4580db76ef2d6cb6a21d99ed3259407ceb1a87ca
yash-devop <kamble1234meena@gmail.com>
2024-12-21 08:57:35 +0530

    add: delete blog server action

---
+++ src/actions/delete-blog.ts
@@ -0,0 +1,41 @@
+"use server";
+import prisma from "@/app/lib/prisma";
+import { revalidatePath } from "next/cache";
+import { redirect, RedirectType } from "next/navigation";
+
+export type State =
+  | { status: "success"; message: string }
+  | {
+      status: "error";
+      message: string;
+      errors?: { name: string; message: string };
+    }
+  | null;
+export async function deleteBlogPage(blogId: string): Promise<State> {
+  try {
+    await prisma.blog.delete({
+      where: {
+        id: blogId,
+      },
+    });
+    // if(deleted){
+    //     revalidatePath("/dashboard")
+    //     // redirect("/dashboard",RedirectType.push)
+    // }
+    return {
+      status: "success",
+      message: "Blog deleted successfully.",
+    };
+  } catch (e) {
+    const error = e as Error;
+    console.log("error in delete-blog action:", error);
+    return {
+      status: "error",
+      message: "Error in deleting the blog.",
+      errors: {
+        name: error.name,
+        message: error.message,
+      },
+    };
+  }
+}
