82c981ec53644be6cea93c06db4d367a01fbc939
yash-devop <kamble1234meena@gmail.com>
2024-12-21 09:03:11 +0530

    add: notfound page for user-facing blog page

---
+++ src/app/blog/[repo]/not-found.tsx
@@ -0,0 +1,13 @@
+import { Link } from "lucide-react";
+
+export default function NotFound() {
+  return (
+    <>
+      <div>
+        <h2>PAGE NOT FOUND</h2>
+        <p>404</p>
+        <Link href="/">Return Home</Link>
+      </div>
+    </>
+  );
+}
