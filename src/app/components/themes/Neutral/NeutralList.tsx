572512cbdc910997c0f2c56f53a9c191997f56bd
yash-devop <kamble1234meena@gmail.com>
2024-12-23 08:46:28 +0530

    add: list issues in themes

---
+++ src/app/components/themes/Neutral/NeutralList.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Badge } from "@/app/components/ui/badge";
+import Link from "next/link";
+import { format } from "date-fns";
+
+type NeutralListProps = {
+  blogData: {
+    id: number;
+    created_at: string;
+    updated_at: string;
+    title: string;
+    state: string;
+    body?: string | null | undefined;
+  }[];
+};
+export default function NeutralList({ blogData }: NeutralListProps) {
+  return (
+    <>
+      <div className="w-full max-w-3xl mx-auto h-full pt-4  px-4 lg:px-0">
+        <p className="text-3xl md:text-4xl py-10">Blog</p>
+        <div className="grid grid-cols-[100px_100px_1fr] w-full gap-y-6">
+          {blogData.map(({ id, body, created_at, state, title }) => {
+            const date = new Date(created_at);
+            const formattedDate = format(date, "MM • yyyy");
+            return (
+              <React.Fragment key={id}>
+                <p className="text-neutral-500 text-base">{formattedDate}</p>
+                <Badge
+                  variant={"default"}
+                  className={`gap-1 w-fit rounded-full ${
+                    state
+                      ? "bg-green-800/30 text-green-600 border-green-500 hover:bg-green-800 cursor-default"
+                      : "bg-neutral-800 text-neutral-600 border-neutral-700  hover:bg-neutral-800 cursor-default"
+                  }`}
+                >
+                  <div 
+                    className={`${
+                      state ? "bg-green-500 animate-pulse" : "bg-neutral-600"
+                    } size-2 rounded-full`}
+                  />
+                  {state ? "open" : "closed"}
+                </Badge>
+                <Link href={`/issues/${id}`} className="truncate">
+                  <span className="text-left  text-neutral-300 underline underline-offset-2 text-base">
+                    {title}
+                  </span>
+                </Link>
+              </React.Fragment>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/app/components/themes/Sahara/SaharaList.tsx
@@ -0,0 +1,48 @@
+import { format } from "date-fns";
+import Link from "next/link";
+import React from "react";
+
+type SaharaListProps = {
+  blogData: {
+    id: number;
+    created_at: string;
+    updated_at: string;
+    title: string;
+    state: string;
+    body?: string | null | undefined;
+  }[];
+};
+
+export default function SaharaList({ blogData }: SaharaListProps) {
+  return (
+    <>
+        <p className="text-3xl md:text-4xl py-10 text-[#111111]">Blog</p>
+      <div className="grid grid-cols-[120px_1fr] w-full md:gap-y-3 gap-x-3 md:gap-x-5">
+
+        {blogData.map(({ body, created_at, id, state, title, updated_at }) => {
+          const date = new Date(created_at);
+          const formattedDate = format(date,"dd - MMM - yyyy")
+          return (
+            <React.Fragment key={id}>
+              <div className="flex flex-col gap-2">
+                <p className="text-[#ad9567] text-base">{formattedDate}</p>
+              </div>
+              <Link
+                href={`/issues/${id}`}
+                className="truncate flex flex-col rounded-lg transition-all duration-300 focus:outline-none"
+              >
+                <div>
+                  <span className="text-left text-[#695b3d] underline underline-offset-2 text-base">
+                    {title}
+                  </span>
+                  {/* <p className="text-neutral-500 truncate">{body}</p> */}
+                </div>
+                {/* <p className="text-[#111111]">Read more</p> */}
+              </Link>
+            </React.Fragment>
+          );
+        })}
+      </div>
+    </>
+  );
+}
