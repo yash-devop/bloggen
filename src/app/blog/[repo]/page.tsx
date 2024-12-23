e55dc364c98f446ff4214891cdd6a0ff551e570c
yash-devop <kamble1234meena@gmail.com>
2024-12-23 08:48:34 +0530

    add: x-theme header and octokit issues sdk

--- src/app/blog/[repo]/page.tsx
+++ src/app/blog/[repo]/page.tsx
@@ -1,19 +1,31 @@
+import Neutral from "@/app/components/themes/Neutral/Neutral";
+import NeutralList from "@/app/components/themes/Neutral/NeutralList";
+import Sahara from "@/app/components/themes/Sahara/Sahara";
+import SaharaList from "@/app/components/themes/Sahara/SaharaList";
 import githubClient from "@/app/lib/octokit";
 import prisma from "@/app/lib/prisma";
+import { headers } from "next/headers";
 import { notFound } from "next/navigation";
+import React from "react";
 
 
 // User Facing Blog Page  ( for everyone ) =>
 
 export default async function UserBlog({
   params,
+  searchParams,
 }: {
   params: Promise<{
     repo: string;
   }>;
+  searchParams: Promise<{
+    theme: "sahara" | "neutral";
+  }>;
 }) {
   const repoId = (await params).repo;
+  const theme = (await headers()).get("x-theme")
   console.log("params: ", await params);
+  console.log("query: ", theme);
   console.log("repoName:", repoId);
   const blog = await prisma.blog.findFirst({
     where: {
@@ -21,94 +33,51 @@ export default async function UserBlog({
     },
   });
 
-  if(!blog){
+  if (!blog) {
     // error.
-    notFound()
+    notFound();
   }
 
   console.log("BLOG OWNER: ", blog?.owner);
   const owner = await prisma.owner.findFirst({
     where: {
-      owner: blog?.owner
-    }
+      owner: blog?.owner,
+    },
   });
 
-  console.log('Owner :',owner);
+  console.log("Owner :", owner);
 
-  if(!owner){
+  if (!owner) {
     // error.
-    notFound()
+    notFound();
   }
 
-  const {installationId} = owner
+  const { installationId } = owner;
 
-  console.log("installationId:", installationId , typeof(installationId));
+  console.log("installationId:", installationId, typeof installationId);
   const octokit = await githubClient.getInstallationOctokit(
     Number(installationId!)
   );
+
+  const issues = await octokit.rest.issues.listForRepo({
+    owner: owner.owner,
+    repo: blog.repoName || repoId
+  })
+
+  // console.log('issues',issues);
+
+  const ListComponent = theme === "neutral" ? NeutralList : SaharaList;
+
   return (
     <>
-      <p>{repoId}</p>
-      <p>Blog page working. with Github INSTALLATION ID : {installationId}</p>
-      {JSON.stringify(blog)}
-      {/* <p>Blog page working. {JSON.stringify(data)}</p> */}
+        {/* <div className="text-red-400">
+          <p>{repoId}</p>
+          <p>
+            Blog page working. with Github INSTALLATION ID : {installationId}
+          </p>
+          {JSON.stringify(blog)}
+        </div> */}
+        <ListComponent blogData={issues.data} />
     </>
   );
 }
-
-
-
-
-
-
-
-
-
-// import githubClient from "@/app/lib/octokit";
-// import prisma from "@/app/lib/prisma";
-// import { notFound } from "next/navigation";
-
-// export default async function UserBlog({
-//   params,
-// }: {
-//   params: Promise<{
-//     repo: string;
-//   }>;
-// }) {
-//   // const data = await prisma.blog.findMany()
-//   const repoName = (await params).repo;
-//   console.log("params: ", await params);
-//   console.log("repoName:", repoName);
-//   const blog = await prisma.blog.findFirst({
-//     where: {
-//       repoName,
-//     },
-//   });
-
-//   console.log("BLOG: ", blog);
-//   const owner = await prisma.owner.findFirst({
-//     where: {
-//       repo: repoName,
-//     },
-//   });
-
-//   if(!owner){
-//     // error.
-//     notFound()
-//   }
-
-//   const {installationId} = owner
-
-//   console.log("installationId:", installationId , typeof(installationId));
-//   const octokit = await githubClient.getInstallationOctokit(
-//     Number(installationId!)
-//   );
-//   return (
-//     <>
-//       <p>{repoName}</p>
-//       <p>Blog page working.</p>
-//       {JSON.stringify(blog)}
-//       {/* <p>Blog page working. {JSON.stringify(data)}</p> */}
-//     </>
-//   );
-// }
