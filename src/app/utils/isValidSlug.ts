2b109ee34c98490c965658a14b0613e61899d3d7
yash-devop <kamble1234meena@gmail.com>
2024-12-23 11:40:46 +0530

    chore: testing subdomain

--- src/app/utils/isValidSlug.ts
+++ src/app/utils/isValidSlug.ts
@@ -1,7 +1,7 @@
 export function isValidSlug(slug?: string): boolean {
     console.log("slug", slug);
     if (!slug) return false;
-    const clients = ["bloggen", "testing"];
+    const clients = ["bloggen", "testing","deploying-nextjs-on-ec2"];
   
     if (clients.includes(slug)) return true;
     return false;
