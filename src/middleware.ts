b4cea573c89dba7b29258582f2944861f553f6d5
yash-devop <kamble1234meena@gmail.com>
2024-12-24 11:55:40 +0530

    revamp: whole multi-tenant middleware logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,16 @@
-import {  NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { auth } from "@/app/utils/auth";
 import { isValidSlug } from "./app/utils/isValidSlug";
 
 // const GET_BLOG_URL = "http://localhost:3000/api/findBlog"; // had to do this bcoz prisma doesnt work in edge env... to get it work , we have to use prisma accelerate.
-const VERCEL_URL = "https://bloggenblog.vercel.app"
-const GET_APP_INSTALLATION = `${VERCEL_URL}/api/checkInstallation`;
+const VERCEL_URL = "https://bloggenblog.yashstack.com";
+const LOCALHOST_URL = "http://localhost:3000";
+const GET_APP_INSTALLATION = `${LOCALHOST_URL}/api/checkInstallation`;
 
 export async function middleware(request: NextRequest) {
+  console.log("MIDDLEWARE RUNNING");
   const url = request.nextUrl;
+  const baseUrl = process.env.NODE_ENV === "production" ? VERCEL_URL : LOCALHOST_URL;
   const BASE_DOMAIN = process.env.NEXT_PUBLIC_ROOT_DOMAIN as string;
   if (request.nextUrl.pathname.startsWith("/dashboard")) {
     console.log("DASHBOARD PAGE running");
@@ -24,54 +27,48 @@ export async function middleware(request: NextRequest) {
         "Content-Type": "application/json",
       },
     });
-    if(!response.ok){   //404
-      console.log(`${response.ok}, installation not found.... in dahsboard mid`);
-      return NextResponse.redirect(new URL("/install",request.url))
+    if (!response.ok) {
+      //404
+      console.log(
+        `${response.ok}, installation not found.... in dahsboard mid`
+      );
+      return NextResponse.redirect(new URL("/install", request.url));
     }
   }
-
   // if (request.nextUrl.pathname.startsWith("/install")) {
   //   console.log('Installa mid work');
   // }
+  let hostname = request.headers
+    .get("host")!
+    .replace(".localhost:3000", `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`);
 
-  const host = request.headers.get("host");
-  const subdomain = host?.split(".")[0];
+  const subdomain = hostname.split(".")[0];
 
-  if (
-    host === BASE_DOMAIN || 
-    subdomain === "www" ||
-    subdomain === BASE_DOMAIN ||
-    url.pathname?.endsWith("/not-found")
-  ) {
-    console.log("pathname unhandled", url.pathname);
-    console.log("subdomain unhandled", subdomain);
-    console.log("BASE_DOMAIN:", BASE_DOMAIN);
-    console.log("subdomain === BASE_DOMAIN", subdomain === BASE_DOMAIN);
-    return NextResponse.next();
+  const searchParams = request.nextUrl.searchParams.toString();
+  // Get the pathname of the request (e.g. /, /about, /blog/first-post)
+  const path = `${url.pathname}${
+    searchParams.length > 0 ? `?${searchParams}` : ""
+  }`;
+  if (hostname === `${BASE_DOMAIN}`) {
+    console.log("url", request.url);
+    console.log('Path in normal route :',`${url.protocol}//${hostname}/blog/${subdomain}${path}`);
+    const response = NextResponse.next();
+    response.headers.set("x-theme", "neutral");
+    return response
   }
 
-  const isValid = isValidSlug(subdomain);
-
-  if (!isValid) {
-    return NextResponse.redirect(
-      new URL(`${url.protocol}//${BASE_DOMAIN}/not-found`)
+  if (hostname.includes(subdomain)) {     // eg: hostname is club-dot-sh.blog.localhost:3000 and subdomain is club-dot-sh
+    console.log("url", request.url);
+    console.log('Path in .localhost:',`${url.protocol}//${hostname}/blog/${subdomain}${path}`);
+    const response = NextResponse.rewrite(
+      new URL(`${url.protocol}${hostname}/blog/${subdomain}${path}`, baseUrl)      // fix this and good to go !
     );
+    response.headers.set("x-theme", "neutral");
+
+    return response
   }
 
-  console.log("values", {
-    url,
-    host,
-    subdomain,
-  });
-
-  const urlWithParams = new URL(
-    `/blog/${subdomain}${url.pathname}${url.search}${url.hash}`,
-    request.url
-  );
-
-  const response = NextResponse.rewrite(urlWithParams);
-  response.headers.set("x-theme", "neutral");      // im setting this for blog theme ( get from database ).
-  return response;
+  return NextResponse.next()
 }
 
 export const config = {
