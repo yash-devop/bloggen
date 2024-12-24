91cf7c6905126b1e737f57b3de1441c498577e8b
yash-devop <kamble1234meena@gmail.com>
2024-12-24 11:56:10 +0530

    not-found

---
+++ src/app/[domain]/blog/[repo]/not-found.tsx
@@ -0,0 +1,16 @@
+import { getTheme } from "@/app/utils/getTheme";
+import { Link } from "lucide-react";
+import { headers } from "next/headers";
+
+export default async function NotFound() {
+  const theme = await getTheme()
+  return (
+    <>
+      <div className={`${theme === "sahara" ? "text-black" : "text-white"}`}>
+        <h2>PAGE NOT FOUND</h2>
+        <p>404</p>
+        <Link href="/">Return Home</Link>
+      </div>
+    </>
+  );
+}
