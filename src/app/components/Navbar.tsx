6169510e92f210e76d3876952fa630d83905a31c
yash-devop <kamble1234meena@gmail.com>
2024-12-28 20:19:41 +0530

    add: navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,20 +1,27 @@
 import Link from "next/link";
 import Bloggen from "./logos/bloggen";
 import { Button } from "./ui/button";
+import { ArrowUpRight } from "lucide-react";
 
-export default function Navbar(){
-    return (
-        <>
-            <header className="border-b border-b-[#DADADA]">
-                <nav className="flex p-3 justify-between items-center max-w-[1700px] mx-auto">
-                    <Bloggen />
-                    <div className="flex gap-2">
-                        <Link href={`/signin`}>
-                            <Button>Login</Button>
-                        </Link>
-                    </div>
-                </nav>
-            </header>
-        </>
-    )
+export default function Navbar() {
+  return (
+    <>
+      <header className="border-bs border-b-[#DADADA] py-4">
+        <nav className="flex py-3 justify-between items-center w-full mx-auto">
+          <Bloggen />
+          <div className="flex gap-2 items-center">
+            <a href={`https://x.com/yash_devop`} className="flex gap-1">
+              <Button variant={"link"} className="p-0">Twitter</Button>
+              <ArrowUpRight size={16} className="shrink-0 p-0"/>
+            </a>
+            <Link href={`/signin`}>
+              <Button className="font-sans border border-[#2b2b2b] bg-black hover:bg-black text-white hover:bg-gradient-to-t transition-all hover:from-black hover:to-[#2b2b2b] focus:bg-gradient-to-t focus:from-gray-700 focus:to-gray-900">
+                Sign in to your account
+              </Button>
+            </Link>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
 }
