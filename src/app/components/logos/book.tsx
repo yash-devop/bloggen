2d81975c36bfcbf8608e10d7420a00ab40d73256
yash-devop <kamble1234meena@gmail.com>
2024-12-29 22:09:17 +0530

    add: blogbook svg

---
+++ src/app/components/logos/book.tsx
@@ -0,0 +1,23 @@
+export const BlogIcon = ({ className }: { className?: string }) => {
+    return (
+      <>
+        <svg
+          width="25"
+          height="25"
+          viewBox="0 0 25 25"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className={className}
+        >
+          <path
+            d="M7.65625 2.34375H6.45833C5.3125 2.34375 4.375 3.28125 4.375 4.42708V20.5729C4.375 21.7187 5.3125 22.6562 6.45833 22.6562H7.65625M7.65625 2.34375V22.6562M7.65625 2.34375H18.5417C19.6875 2.34375 20.625 3.28125 20.625 4.42708V20.5729C20.625 21.7187 19.6875 22.6562 18.5417 22.6562H7.65625"
+            stroke="#E9435C"
+            strokeWidth="0.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      </>
+    );
+  };
+  
