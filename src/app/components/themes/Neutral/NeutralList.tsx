08413e2e0892201186351fc7c11e84d03ab21b1e
yash-devop <kamble1234meena@gmail.com>
2025-01-16 19:41:30 +0530

    chore: replace id into number

--- src/app/components/themes/Neutral/NeutralList.tsx
+++ src/app/components/themes/Neutral/NeutralList.tsx
@@ -5,7 +5,7 @@ import { format } from "date-fns";
 
 type NeutralListProps = {
   blogData: {
-    id: number;
+    number: number;
     created_at: string;
     updated_at: string;
     title: string;
@@ -19,11 +19,11 @@ export default function NeutralList({ blogData }: NeutralListProps) {
       <div className="w-full max-w-3xl mx-auto h-full pt-4  px-4 lg:px-0">
         <p className="text-3xl md:text-4xl py-10">Blog</p>
         <div className="grid grid-cols-[100px_100px_1fr] w-full gap-y-6">
-          {blogData.map(({ id, created_at, state, title }) => {
+          {blogData && blogData.length > 0  ?  blogData.map(({ number, created_at, state, title }) => {
             const date = new Date(created_at);
             const formattedDate = format(date, "dd-MM-yyyy");
             return (
-              <React.Fragment key={id}>
+              <React.Fragment key={number}>
                 <p className="text-neutral-500 text-base">{formattedDate}</p>
                 <Badge
                   variant={"default"}
@@ -40,14 +40,14 @@ export default function NeutralList({ blogData }: NeutralListProps) {
                   />
                   {state ? "open" : "closed"}
                 </Badge>
-                <Link href={`/issues/${id}`} className="truncate">
+                <Link href={`/issues/${number}`} className="truncate">
                   <span className="text-left  text-neutral-300 underline underline-offset-2 text-base">
                     {title}
                   </span>
                 </Link>
               </React.Fragment>
             );
-          })}
+          }): <p className="text-neutral-500 py-10">No issues found in your repository.</p>}
         </div>
       </div>
     </>
