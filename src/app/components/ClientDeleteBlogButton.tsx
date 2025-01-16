8ac1c5b8836e0d70dc915cf632137877e7dea06c
yash-devop <kamble1234meena@gmail.com>
2025-01-16 13:01:29 +0530

    client auth button

--- src/app/components/ClientDeleteBlogButton.tsx
+++ src/app/components/ClientDeleteBlogButton.tsx
@@ -1,26 +1,31 @@
-"use client"
+"use client";
 import { deleteBlogPage } from "@/actions/delete-blog";
 import { useParams, useRouter } from "next/navigation";
+import { Button } from "./ui/button";
+import { useState } from "react";
 
+export default function DeleteButton() {
+  const { repo } = useParams();
+  const repoId = repo as string;
+  const router = useRouter();
+  const [isSubmitting , setIsSubmitting ] = useState(false)
 
-export default function DeleteButton(){
-    const {repo}= useParams();
-    const repoId = repo as string
-    const router = useRouter();
-
-    const handleDelete = async () => {
-        try {
-          // Assuming deleteBlogPage is an async function
-          await deleteBlogPage(repoId); // No need to cast `repo` as string
-          router.push("/dashboard");
-        } catch (error) {
-          console.error("Failed to delete blog page:", error);
-          // Optionally, you can display an error message to the user here
-        }
-      };
-    return (
-        <>
-        <button onClick={handleDelete} className="p-5 bg-red-400">DELETE BLOG ?</button>
-        </>
-    )
+  const handleDelete = async () => {
+    try {
+      setIsSubmitting(true)
+      // Assuming deleteBlogPage is an async function
+      await deleteBlogPage(repoId); // No need to cast `repo` as string
+      router.push("/dashboard");
+    } catch (error) {
+      console.error("Failed to delete blog page:", error);
+      // Optionally, you can display an error message to the user here
+    }
+  };
+  return (
+    <>
+      <Button onClick={handleDelete} disabled={isSubmitting} className="bg-destructive rounded-lg disabled:bg-primary/50">
+        Delete Blog
+      </Button>
+    </>
+  );
 }
