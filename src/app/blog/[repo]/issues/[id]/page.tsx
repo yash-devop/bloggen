c4fb87921ff25cae7e0a4179ce05d2df17ee796b
yash-devop <kamble1234meena@gmail.com>
2025-01-16 19:41:40 +0530

    add: issues by id page

--- src/app/blog/[repo]/issues/[id]/page.tsx
+++ src/app/blog/[repo]/issues/[id]/page.tsx
@@ -1,24 +1,110 @@
-import Image from "next/image"
+import { notFound } from "next/navigation";
+import githubClient from "@/app/lib/octokit";
+import { decodeRepoName } from "@/app/utils/modifyRepo";
+import prisma from "@/app/lib/prisma";
 
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+
+import MarkdownComponent from "./MarkdownComponent";
+import { getTheme } from "@/app/utils/getTheme";
+import { Button } from "@/app/components/ui/button";
+import Link from "next/link";
 export default async function IssuePage({
-    params,
-    searchParams
-}:{
-    params: Promise<{
-        id: string
-    }>,
-    searchParams: Promise<{
-        theme: "sahara" | "neutral";
-      }>
-}){
-    const themeQuery = (await searchParams).theme;
-    const query = (await params).id
-    // const IssueComponent = themeQuery === "neutral" ? NeutralList : SaharaList         // change it with issue component.
+  params,
+}: {
+  params: Promise<{
+    repo: string;
+    id: string;
+  }>
+}) {
+  const theme = await getTheme();
+  const issueId = (await params).id;
+  const repoId = (await params).repo;
+  console.log("theme", theme);
+
+  const repoName = decodeRepoName(repoId);
+  console.log("reponame", repoName);
+  const blog = await prisma.blog.findFirst({
+    where: {
+      repoName,
+    },
+  });
+
+  if (!blog) {
+    notFound();
+  }
+
+  console.log("BLOG OWNER: ", blog?.owner);
+  const owner = await prisma.owner.findFirst({
+    where: {
+      owner: blog?.owner,
+    },
+  });
+
+  console.log("Owner :", owner);
+
+  if (!owner) {
+    notFound();
+  }
+
+  const { installationId } = owner;
+  const octokit = await githubClient.getInstallationOctokit(
+    Number(installationId!)
+  );
+
+  try {
+    const issues = await octokit.rest.issues.get({
+      owner: owner.owner,
+      repo: blog.repoName || repoId,
+      issue_number: Number(issueId),
+    });
+
+    const comments = await octokit.rest.issues.listComments({
+      owner: owner.owner,
+      repo: blog.repoName || repoId,
+      issue_number: Number(issueId),
+    });
+    //   const ListComponent = theme === "neutral" ? NeutralList : SaharaList;
+
+    console.log('comments',comments);
     return (
-        <>
-                <p>ACTIVE THEME: {themeQuery}</p>
-                <p>Im issue page {JSON.stringify(query)}</p>
-                <Image alt="demo-img" src={"https://swiperjs.com/demos/images/nature-1.jpg"} width={30} height={30} unoptimized className="w-full object-contain h-full"/>
-        </>
-    )
+      <>
+        <div className="flex flex-col">
+        <div className="flex justify-between flex-wrap gap-4 pb-16">
+          <h1 className={`text-3xl ${
+          theme === "sahara" ? "text-black" : "text-white"
+        }`}>testing-react-redux-toolkit</h1>
+          <div className="">
+            <Button className={"text-base"}>
+                <a target="_blank" href={`https://github.com/${owner.owner}/${repoName}/issues/${issueId}`}>
+                Leave a comment ?
+                </a>
+            </Button>
+          </div>
+        </div>
+          {comments && comments.data.length > 0 ? (
+            comments.data.map((comment , idx) => {
+              return (
+                <>
+                  <div className="flex flex-col gap-6">
+                    <div className={`w-full border-b border-neutral-700 py-4 ${
+          theme === "sahara" ? "text-black" : "text-white"
+        }`}>
+                        <p>Comment {idx+1}</p>
+                    </div>
+                    <MarkdownComponent markdown={comment.body!} theme={theme} />
+                  </div>
+                </>
+              );
+            })
+          ) : (
+            <p className="text-center py-10 text-neutral-600">No comments on this issue...wowowowow</p>
+          )}
+        </div>
+      </>
+    );
+  } catch (error) {
+    console.log("error", error);
+    notFound();
+  }
 }
